f259ee1e3360183f379f7af23a53c051a334427b
daniel-j-dev <danielaugustusjohnson@gmail.com>
2020-12-19 22:01:41 -0500

    Setup components and styles for mobile

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PropertyTypeComponent } from './components/property-type/property-type.component';
+import { PriceRangeComponent } from './components/price-range/price-range.component';
+import { ResultsComponent } from './components/results/results.component';
+import { ResultItemComponent } from './components/result-item/result-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    HeaderComponent,
+    PropertyTypeComponent,
+    PriceRangeComponent,
+    ResultsComponent,
+    ResultItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
