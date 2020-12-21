7271dea6652da6c103696acbc72c879d6b5d9a67
daniel-j-dev <danielaugustusjohnson@gmail.com>
2020-12-20 23:37:52 -0500

    Styled buttons

--- src/app/components/price-range/price-range.component.ts
+++ src/app/components/price-range/price-range.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Options } from '@angular-slider/ngx-slider';
 
 @Component({
   selector: 'app-price-range',
   templateUrl: './price-range.component.html',
-  styleUrls: ['./price-range.component.css']
+  styleUrls: ['./price-range.component.css'],
 })
 export class PriceRangeComponent implements OnInit {
+  value: number = 954;
+  highValue: number = 1200;
+  options: Options = {
+    floor: 846,
+    ceil: 1392,
+    hideLimitLabels: true,
+  };
 
-  constructor() { 
-    
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
